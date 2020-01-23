692e4fca14c69368dc223246f6be22e8799cd7f3
freddy <freddy03h@gmail.com>
2020-01-23 12:18:49 +0100

    fix handle .web files and node include react-native* dependencies

--- razzle.config.js
+++ razzle.config.js
@@ -1,29 +1,33 @@
-'use strict';
+"use strict";
 
 // in order for SSR to work, the react-native-web babel transform needs to be
 // run on any code that references 'react-native'. So, we're overriding razzle's
 // default "externals" configuration to also whitelist any deps that start with
-// 'bs-react-native'. This will add those deps to the server bundle, but should
+// 'react-native'. This will add those deps to the server bundle, but should
 // also run the babel transform on them to switch 'react-native' --> 'react-native-web'
 
-const nodeExternals = require('webpack-node-externals');
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   modify: (config, { target, dev }) => {
-    if (target === 'node') {
+    config.resolve.extensions = config.resolve.extensions
+      .map(extension => ".web" + extension)
+      .concat(config.resolve.extensions);
+
+    if (target === "node") {
       config.externals = [
         nodeExternals({
           whitelist: [
-            dev ? 'webpack/hot/poll?300' : null,
+            dev ? "webpack/hot/poll?300" : null,
             /\.(eot|woff|woff2|ttf|otf)$/,
             /\.(svg|png|jpg|jpeg|gif|ico)$/,
             /\.(mp4|mp3|ogg|swf|webp)$/,
             /\.(css|scss|sass|sss|less)$/,
-            mod => mod.indexOf('bs-react-native') === 0,
-          ].filter(x => x),
-        }),
+            mod => mod.indexOf("react-native") === 0
+          ].filter(x => x)
+        })
       ];
     }
     return config;
-  },
+  }
 };
